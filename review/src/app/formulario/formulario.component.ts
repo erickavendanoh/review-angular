23dc8b54fb2d181d896d3901cb97c8557e794a58
Erick Avendaño <117497394+erickavendanoh@users.noreply.github.com>
2024-07-13 15:45:18 -0600

    Creacion de primeros componentes y un modelo. Pruebas de property, event y two-way binding

---
+++ review/src/app/formulario/formulario.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css'
+})
+export class FormularioComponent {
+  nombreInput:string = '';
+  apellidoInput:string = '';
+
+  mostrar(){
+    console.log(this.nombreInput);
+  }
+
+}
---
+++ review/src/app/persona.model.ts
@@ -0,0 +1,11 @@
+class Persona{
+    // nombre:string = '';
+    // apellido:string = '';
+
+    // constructor(nombreP: string, apellidoP:string){
+    //     this.nombre = nombreP;
+    //     this.apellido = apellidoP;
+    // }
+    
+    constructor(public nombre:string, public apellido:string){}
+}
